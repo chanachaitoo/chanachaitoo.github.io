51bac70969b8b96f18b411d18f4e26051865bb63
chanachaitoo <124030234+chanachaitoo@users.noreply.github.com>
2025-12-01 13:00:18 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -195,6 +195,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.05);
                 display: flex;
                 justify-content: center; 
+                z-index: 1000;
             }
 
             .nav-container {
